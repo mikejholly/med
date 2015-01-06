1f92bb112973df2cf7d03661f6e8af193f6fd6a4
Mike Holly <mikejholly@gmail.com>
2015-01-06 11:48:42 -0800

    Refactor

--- src/med.c
+++ src/med.c
@@ -10,14 +10,14 @@
 void med_state_init(med_state_t *state) {
   med_state_open(state);
   med_state_termios(state);
-  med_state_get_size(state);
-  med_state_init_cells(state);
 
-  med_state_write(state, funcs[MED_FUNC_ENTER_CA]);
-  med_state_write(state, funcs[MED_FUNC_ENTER_KEYPAD]);
-  med_state_write(state, funcs[MED_FUNC_HIDE_CURSOR]);
+  med_screen_init(&state->screen, state->tty_fd);
 
-  med_state_clear(state);
+  med_buffer_init(&state->buffer, 2048);
+  med_buffer_puts(&state->buffer, MED_FUNC_ENTER_CA);
+  med_buffer_puts(&state->buffer, MED_FUNC_ENTER_KEYPAD);
+  med_buffer_puts(&state->buffer, MED_FUNC_HIDE_CURSOR);
+
   med_state_render(state);
 }
 
@@ -44,45 +44,66 @@ void med_state_termios(med_state_t *state) {
   tcsetattr(state->tty_fd, TCSAFLUSH, &tios);
 }
 
-void med_state_get_size(med_state_t *state) {
-  struct winsize sz;
-
-  ioctl(state->tty_fd, TIOCGWINSZ, &sz);
-
-  state->height = sz.ws_col;
-  state->width = sz.ws_row;
+void med_cells_init(med_cell_t *cells, int x, int y) {
+  int n = x * y;
+  cells = (med_cell_t *)malloc(n * sizeof(med_cell_t));
 }
 
-void med_state_init_cells(med_state_t *state) {
-  int n = state->width * state->height;
-  state->cells = (med_cell_t*)malloc(n * sizeof(med_cell_t));
-}
-
-void med_state_set_cell(med_state_t *state, int x, int y, med_cell_t *cell) {
-  int n = (state->width * y + x);
-  state->cells[n] = *cell;
-}
-
-void med_state_clear(med_state_t *state) {
+void med_cells_clear(med_cell_t *cells, int width, int height) {
   int i;
-  int n = state->width * state->height;
+  int n = width * height;
   for (i = 0; i < n; i++) {
-    state->cells[i].ch = ' ';
-    state->cells[i].fg = 0;
-    state->cells[i].bg = 0;
+    cells[i].ch = ' ';
+    cells[i].fg = 0;
+    cells[i].bg = 0;
   }
 }
 
 void med_state_render(med_state_t *state) {
   int i;
-  int n = state->width * state->height;
-  med_state_buffer
-  med_state_write(state, funcs[MED_FUNC_CLEAR_SCREEN]);
-  med_state_write(state, str);
+  int n = state->screen.width * state->screen.height;
+  med_buffer_clear(&state->buffer);
+  med_buffer_puts(&state->buffer, MED_FUNC_CLEAR_SCREEN);
 }
 
-void med_state_write(med_state_t *state, char *str) {
-  write(state->tty_fd, str, strlen(str));
+void med_buffer_init(med_buffer_t *buffer, int cap) {
+  buffer->len = 0;
+  buffer->cap = cap;
+  buffer->buf = (char *)malloc(cap * sizeof(char *));
+}
+
+void med_buffer_grow(med_buffer_t *buffer) {
+  int len = buffer->cap * 2;
+  buffer->buf = (char *)realloc(buffer->buf, len * sizeof(char *));
+  buffer->cap = len;
+}
+
+void med_buffer_clear(med_buffer_t *buffer) {
+  buffer->len = 0;
+}
+
+void med_buffer_puts(med_buffer_t *buffer, char *str) {
+  int extra = strlen(str);
+  if (buffer->len + extra) {
+    med_buffer_grow(buffer);
+  }
+}
+
+void med_screen_init(med_screen_t *screen, int fd) {
+  med_screen_update_size(screen, fd);
+  med_cells_init(screen->cells, screen->width, screen->height);
+}
+
+void med_screen_update_size(med_screen_t *screen, int fd) {
+  struct winsize sz;
+  ioctl(fd, TIOCGWINSZ, &sz);
+  screen->height = sz.ws_col;
+  screen->width = sz.ws_row;
+}
+
+void med_screen_set(med_screen_t *screen, int x, int y, med_cell_t *cell) {
+  int n = (screen->width * y + x);
+  screen->cells[n] = *cell;
 }
 
 int main(int argc, char **argv) {
--- src/med.h
+++ src/med.h
@@ -4,6 +4,7 @@
 typedef struct {
   int x;
   int y;
+  int hide;
 } med_cursor_t;
 
 typedef enum {
@@ -32,79 +33,76 @@ typedef struct {
 } med_cell_t;
 
 typedef struct {
+  int len;
+  int cap;
+  char *buf;
+} med_buffer_t;
+
+typedef struct {
+  med_cell_t *cells;
+  int width;
+  int height;
+} med_screen_t;
+
+typedef struct {
   med_cursor_t cursor;
   med_mode_t mode;
   med_history_t history;
-  med_cell_t *cells;
+  med_screen_t screen;
+  med_buffer_t buffer;
   int tty_fd;
-  int width;
-  int height;
 } med_state_t;
 
 void med_state_init(med_state_t *state);
 void med_state_open(med_state_t *state);
 void med_state_termios(med_state_t *state);
-void med_state_get_size(med_state_t *state);
-void med_state_init_cells(med_state_t *state);
-void med_set_cell(med_state_t *state, med_cell_t *cell);
-void med_clear_cells(med_state_t *state);
-void med_state_set_cell(med_state_t *state, int x, int y, med_cell_t *cell);
-void med_state_clear(med_state_t *state);
+void med_state_update_size(med_state_t *state);
 void med_state_render(med_state_t *state);
-void med_state_write(med_state_t *state, char *str);
 
-char *keys[] = {
-  "\033OP",
-  "\033OQ",
-  "\033OR",
-  "\033OS",
-  "\033[15~",
-  "\033[17~",
-  "\033[18~",
-  "\033[19~",
-  "\033[20~",
-  "\033[21~",
-  "\033[23~",
-  "\033[24~",
-  "\033[2~",
-  "\033[3~",
-  "\033OH",
-  "\033OF",
-  "\033[5~",
-  "\033[6~",
-  "\033OA",
-  "\033OB",
-  "\033OD",
-  "\033OC",
-  0,
-};
+void med_screen_init(med_screen_t *screen, int fd);
+void med_screen_update_size(med_screen_t *screen, int fd);
+void med_screen_set(med_screen_t *screen, int x, int y, med_cell_t *cell);
 
-enum {
-  MED_FUNC_ENTER_CA,
-  MED_FUNC_EXIT_CA,
-  MED_FUNC_SHOW_CURSOR,
-  MED_FUNC_HIDE_CURSOR,
-  MED_FUNC_CLEAR_SCREEN,
-  MED_FUNC_SGR0,
-  MED_FUNC_UNDERLINE,
-  MED_FUNC_BOLD,
-  MED_FUNC_BLINK,
-  MED_FUNC_REVERSE,
-  MED_FUNC_ENTER_KEYPAD,
-  MED_FUNC_EXIT_KEYPAD,
-};
+void med_cells_init(med_cell_t *cells, int width, int height);
+void med_cells_clear(med_cell_t *cells, int width, int height);
 
-char *funcs[] = {
-  "\033[?1049h",
-  "\033[?1049l",
-  "\033[?12l\033[?25h",
-  "\033[?25l",
-  "\033[H\033[2J",
-  "\033(B\033[m",
-  "\033[4m",
-  "\033[1m",
-  "\033[5m",
-  "\033[7m",
-  "\033[?1h\033=",
-  "\033[?1l\033>",
-};
+void med_buffer_init(med_buffer_t *buffer, int cap);
+void med_buffer_clear(med_buffer_t *buffer);
+void med_buffer_grow(med_buffer_t *buffer);
+void med_buffer_puts(med_buffer_t *buffer, char * str);
+
+#define MED_KEY_F1     "\033OP"
+#define MED_KEY_F2     "\033OQ"
+#define MED_KEY_F3     "\033OR"
+#define MED_KEY_F4     "\033OS"
+#define MED_KEY_F5     "\033[15~"
+#define MED_KEY_F6     "\033[17~"
+#define MED_KEY_F7     "\033[18~"
+#define MED_KEY_F8     "\033[19~"
+#define MED_KEY_F9     "\033[20~"
+#define MED_KEY_F10    "\033[21~"
+#define MED_KEY_F11    "\033[23~"
+#define MED_KEY_F12    "\033[24~"
+#define MED_KEY_INSERT "\033[2~"
+#define MED_KEY_DELETE "\033[3~"
+#define MED_KEY_HOME   "\033OH"
+#define MED_KEY_END    "\033OF"
+#define MED_KEY_PGUP   "\033[5~"
+#define MED_KEY_PGDN   "\033[6~"
+#define MED_KEY_UP     "\033OA"
+#define MED_KEY_DOWN   "\033OB"
+#define MED_KEY_LEFT   "\033OD"
+#define MED_KEY_RIGHT  "\033OC"
+
+#define MED_FUNC_ENTER_CA     "\033[?1049h"
+#define MED_FUNC_EXIT_CA      "\033[?12l\033[?25h"
+#define MED_FUNC_SHOW_CURSOR  "\033[?12l\033[?25h"
+#define MED_FUNC_HIDE_CURSOR  "\033[?25l"
+#define MED_FUNC_CLEAR_SCREEN "\033[H\033[2J"
+#define MED_FUNC_SGR0         "\033(B\033[m"
+#define MED_FUNC_UNDERLINE    "\033[4m"
+#define MED_FUNC_BOLD         "\033[1m"
+#define MED_FUNC_BLINK        "\033[5m"
+#define MED_FUNC_REVERSE      "\033[7m"
+#define MED_FUNC_ENTER_KEYPAD "\033[?1h\033="
+#define MED_FUNC_EXIT_KEYPAD  "\033[?1l\033>"
